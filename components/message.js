05e746a11b391ad1af806da133a5d3b227b238be
noahsploiter <noahsploit@gmail.com>
2023-04-17 16:12:00 +0300

    'test'

--- components/message.js
+++ components/message.js
@@ -1,4 +1,14 @@
-export default function Message({ children, avatar, username, description }) {
+export default function Message({
+  children,
+  avatar,
+  username,
+  description,
+  phoneNumber,
+  idNumber,
+  cardModel,
+  freeSpace,
+  price,
+}) {
   return (
     <div className="bg-white p-8 border-b-2 rounded-lg">
       <div className="flex items-center gap-2">
@@ -6,7 +16,12 @@ export default function Message({ children, avatar, username, description }) {
         <h2>{username}</h2>
       </div>
       <div className="py-4">
-        <p>{description}</p>
+        <p>ስም: {description}</p>
+        <p>ስልክ: {phoneNumber}</p>
+        <p>የተሽከርካሪ አይነት: {cardModel}</p>
+        <p>የታርጋ ቁጥር፡ {idNumber}</p>
+        <p>ክፍት ቦታ፡ {freeSpace}</p>
+        <p>ዋጋ፡ {price}</p>
       </div>
       {children}
     </div>
--- pages/post.js
+++ pages/post.js
@@ -14,7 +14,11 @@ import { toast } from "react-toastify";
 export default function Post() {
   //Form state
   const [name, setName] = useState({ description: "" });
-  const [number, setNumber] = useState({ description: "" });
+  const [phone, setPhone] = useState({ phoneNumber: "" });
+  const [cardModel, setCardModel] = useState({ cardModel: "" });
+  const [idNumber, setIdNumber] = useState({ idNumber: "" });
+  const [freeSpace, setFreeSpace] = useState({ freeSpace: "" });
+  const [price, setPrice] = useState({ price: "" });
   const [user, loading] = useAuthState(auth);
   const route = useRouter();
   const routeData = route.query;
@@ -24,14 +28,28 @@ export default function Post() {
     e.preventDefault();
     //Run checks for description
     if (!name.description) {
-      toast.error("Description Field empty 😅", {
+      toast.error("Full Name field empty 😅", {
         position: toast.POSITION.TOP_CENTER,
         autoClose: 1500,
       });
       return;
     }
-    if (name.description.length > 300) {
-      toast.error("Description too long 😅", {
+    if (!phone.phoneNumber) {
+      toast.error("Phone Number field empty 😅", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 1500,
+      });
+      return;
+    }
+    if (!cardModel.cardModel) {
+      toast.error("Phone Number field empty 😅", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 1500,
+      });
+      return;
+    }
+    if (!idNumber.idNumber) {
+      toast.error("ID Number field empty 😅", {
         position: toast.POSITION.TOP_CENTER,
         autoClose: 1500,
       });
@@ -40,7 +58,15 @@ export default function Post() {
 
     if (name?.hasOwnProperty("id")) {
       const docRef = doc(db, "posts", name.id);
-      const updatedPost = { ...name, timestamp: serverTimestamp() };
+      const updatedPost = {
+        ...name,
+        ...phone,
+        ...idNumber,
+        ...cardModel,
+        ...freeSpace,
+        ...price,
+        timestamp: serverTimestamp(),
+      };
       await updateDoc(docRef, updatedPost);
       return route.push("/");
     } else {
@@ -48,12 +74,22 @@ export default function Post() {
       const collectionRef = collection(db, "posts");
       await addDoc(collectionRef, {
         ...name,
+        ...phone,
+        ...idNumber,
+        ...cardModel,
+        ...freeSpace,
+        ...price,
         timestamp: serverTimestamp(),
         user: user.uid,
         avatar: user.photoURL,
         username: user.displayName,
       });
       setName({ description: "" });
+      setPhone({ phoneNumber: "" });
+      setCardModel({ cardModel: "" });
+      setIdNumber({ idNumber: "" });
+      setFreeSpace({ freeSpace: "" });
+      setPrice({ price: "" });
       toast.success("Post has been made 🚀", {
         position: toast.POSITION.TOP_CENTER,
         autoClose: 1500,
@@ -68,6 +104,11 @@ export default function Post() {
     if (!user) route.push("/auth/login");
     if (routeData.id) {
       setName({ description: routeData.description, id: routeData.id });
+      setPhone({ phoneNumber: routeData.phoneNumber, id: routeData.id });
+      setCardModel({ cardModel: routeData.cardModel, id: routeData.id });
+      setIdNumber({ idNumber: routeData.idNumber, id: routeData.id });
+      setFreeSpace({ freeSpace: routeData.freeSpace, id: routeData.id });
+      setPrice({ price: routeData.freeSpace, id: routeData.id });
     }
   };
 
@@ -77,37 +118,107 @@ export default function Post() {
 
   return (
     <div className="my-20 p-12 shadow-lg rounded-lg max-w-md mx-auto">
-      <form onSubmit={submitPost}>
+      <from onSubmit={submitPost}>
         <h1 className="text-2xl font-bold">
           {name.hasOwnProperty("id") ? "Edit your post" : "Create a new post"}
         </h1>
-        <div className="py-2">
+        <form className="py-2">
           <div>
-            <h3 className="text-lg font-medium py-2">Full Name</h3>
-            <textarea
-              placeholder="Full Name, Phone number"
+            <h3 className="text-lg font-medium py-2">ሙሉ ስም፡</h3>
+            <input
+              placeholder="ሙሉ ስም"
               value={name.description}
               onChange={(e) =>
-                setName({ ...name, description: e.target.value })
+                setName({
+                  ...name,
+                  description: e.target.value,
+                })
               }
-              className="bg-gray-800 h-[100px] w-full text-white rounded-lg p-2 text-sm"
+              className="bg-gray-800 h-[60px] w-full text-white rounded-lg p-2 text-sm"
             />
           </div>
-          <p
-            className={`text-cyan-600 font-medium text-sm ${
-              name.description.length > 300 ? "text-red-600" : ""
-            }`}
+          <div>
+            <h3 className="text-lg font-medium py-2">ስልክ ቁጥር፡</h3>
+            <input
+              placeholder="ስልክ ቁጥር"
+              value={phone.phoneNumber}
+              onChange={(e) =>
+                setPhone({
+                  ...phone,
+                  phoneNumber: e.target.value,
+                })
+              }
+              className="bg-gray-800 h-[60px] w-full text-white rounded-lg p-2 text-sm"
+            />
+          </div>
+          <div>
+            <h3 className="text-lg font-medium py-2">የተሽከርካሪ አይነት</h3>
+            <input
+              placeholder="የተሽከርካሪ አይነት"
+              value={cardModel.cardModel}
+              onChange={(e) =>
+                setCardModel({
+                  ...cardModel,
+
+                  cardModel: e.target.value,
+                })
+              }
+              className="bg-gray-800 h-[60px] w-full text-white rounded-lg p-2 text-sm"
+            />
+          </div>
+          <div>
+            <h3 className="text-lg font-medium py-2">የታርጋ ቁጥር፡</h3>
+            <input
+              placeholder="የታርጋ ቁጥር"
+              value={idNumber.idNumber}
+              onChange={(e) =>
+                setIdNumber({
+                  ...idNumber,
+
+                  idNumber: e.target.value,
+                })
+              }
+              className="bg-gray-800 h-[60px] w-full text-white rounded-lg p-2 text-sm"
+            />
+          </div>
+          <div>
+            <h3 className="text-lg font-medium py-2">ክፍት ቦታ፡</h3>
+            <input
+              placeholder="ክፍት ቦታ"
+              value={freeSpace.freeSpace}
+              onChange={(e) =>
+                setFreeSpace({
+                  ...freeSpace,
+
+                  freeSpace: e.target.value,
+                })
+              }
+              className="bg-gray-800 h-[60px] w-full text-white rounded-lg p-2 text-sm"
+            />
+          </div>
+          <div>
+            <h3 className="text-lg font-medium py-2">ዋጋ፡</h3>
+            <input
+              placeholder="ክፍት ቦታ"
+              value={price.price}
+              onChange={(e) =>
+                setPrice({
+                  ...price,
+
+                  price: e.target.value,
+                })
+              }
+              className="bg-gray-800 h-[60px] w-full text-white rounded-lg p-2 text-sm"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-cyan-600 text-white font-medium p-2 my-2 rounded-lg text-sm"
           >
-            {name.description.length}/300
-          </p>
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-cyan-600 text-white font-medium p-2 my-2 rounded-lg text-sm"
-        >
-          Submit
-        </button>
-      </form>
+            Submit
+          </button>
+        </form>
+      </from>
     </div>
   );
 }
